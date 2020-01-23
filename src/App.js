d27113eee10bb03863224466e70b414a8f71f204
Kate Miller <kat3miller@gmail.com>
2020-01-23 15:51:56 -0800

    Create sidebar for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Parallax from './components/Parallax/Parallax';
+import Sidebar from './components/Sidebar/Sidebar';
 
 
 function App() {
   return (
     <div className="App">
+      <Sidebar/>
       <Parallax/>
     </div>
   );
